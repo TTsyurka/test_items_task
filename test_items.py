b2d14a6830d2bcdf7f59f5eadae4e70de91973ac
Taras Tsyurka <122723728+TTsyurka@users.noreply.github.com>
2023-07-23 15:55:15 +0500

    minor tweaks

--- test_items.py
+++ test_items.py
@@ -4,9 +4,10 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_basket(browser):
+def test_basket_button_existed(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    basket = browser.find_element(By.CSS_SELECTOR, 'div.basket-mini.pull-right.hidden-xs > span > a')
-    print(basket.text)
-    time.sleep(15)
+    basket = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
+    print('\n', basket.text)
+    assert basket != None, "the button is missing"
+    time.sleep(30)
